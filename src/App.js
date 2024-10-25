f71578ad8a4ab492ca556ed5c1766a1fe4730da3
Rebeca Gois <rebecagoismachado@gmail.com>
2024-10-25 13:57:00 -0300

    Corrige rotas e adiciona redirecionamento para /Vitrine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate  } from "react-router-dom";
 import Vitrine from "./pages/Vitrine";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/vitrine" element={<Vitrine />} />
+      <Route path="/" element={<Navigate to="/Vitrine" />} />
+
     </Routes>
   </Router>
 );
